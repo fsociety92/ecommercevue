634d69cd41b470e60a007e7f1654f89b40d8fa13
fscoiety92 <emil.1emil92@gmail.com>
2022-06-23 06:46:56 +0200

    cart

---
+++ src/views/Cart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-for="p in productsCart" :key="p?.id">
+      <p>{{p?.title}}</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { useProductsStore } from "../stores/productCart";
+import { onMounted } from "vue";
+
+export default {
+  setup() {
+    const store = useProductsStore();
+    const getProducts = store.getProductsCart;
+    const productsCart = store.productsCart;
+
+   console.log(productsCart);
+
+    onMounted(() => {
+      store.fetchProductsCart();
+
+    });
+
+    return {
+      getProducts,
+      productsCart,
+
+
+    };
+  },
+};
+</script>
+
+<style>
+</style>
